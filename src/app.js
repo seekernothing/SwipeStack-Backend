fac723ec58c4adbc4d7367e0ebb2d10e6cd5d1d0
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-04-17 17:09:45 +0530

    tried routs

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.get("/", (req, res) => {
-  res.send("Server is working!");
+app.get("/users/:userId/:name/:password", (req, res) => {
+    console.log(req.params)
+  res.send({firstName:"Abhishek",lastname:"Biradar"});
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
