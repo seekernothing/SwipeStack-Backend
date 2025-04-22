6ed62de42675d2aad12cc0fd5f8c29439f0067f3
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-04-22 20:38:51 +0530

    add auth middleware

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
+const { adminAuth,userAuth } = require("./middlewares/auth");
 
-app.get("/users/:userId/:name/:password", (req, res) => {
-    console.log(req.params)
-  res.send({firstName:"Abhishek",lastname:"Biradar"});
+app.use("/admin",adminAuth);
+
+app.get("/user",userAuth, (req, res) => {
+  res.send("Sent the user data");
 });
 
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All data sent");
+});
 
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted the user");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ src/middlewares/auth.js
@@ -0,0 +1,26 @@
+const adminAuth = (req, res, next) => {
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("Unauthorized request");
+  } else {
+    next();
+  }
+};
+
+// User Auth
+
+const userAuth = (req, res, next) => {
+  const token = "xyz";
+  const isuserAuthorized = token === "xyz";
+  if (!isuserAuthorized) {
+    res.status(401).send("Unauthorized request");
+  } else {
+    next();
+  }
+};
+
+module.exports={
+    adminAuth,
+    userAuth
+}
