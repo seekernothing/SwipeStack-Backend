26abcefa8f8eba16d2e72a23a9f90c5d23cfe169
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-04-23 22:02:09 +0530

    implement user model with validation and CRUD operations in app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,78 @@
 const express = require("express");
-const connectDB = require("./config/database");
 const app = express();
 const PORT = 8000;
+const connectDB = require("./config/database");
 const User = require("./models/user");
+const user = require("./models/user");
 
-app.use(express.json())
+app.use(express.json());
 
+// Signup api  🔴( data sanitization needed in post and patch apis)
 app.post("/signup", async (req, res) => {
   // creating a new instance of the User model
-  console.log(req.body)
+  console.log(req.body);
   const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User added sucessfully");
   } catch (error) {
-    res.status(400).send("Error saving the user " + error.message)
+    res.status(400).send("Error saving the user " + error.message);
+  }
+});
+
+//Feed api - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+
+  try {
+    const users = await User.find({ email: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    }
+    res.send(users);
+  } catch (error) {
+    res.status(401).send("Something went wrong");
+  }
+});
+
+// api to delete user by id
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    // const user = await User.findByIdAndDelete({_id: userId})
+    const user = await User.findByIdAndDelete(userId); // this one is shorthand for upper-one
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// api to update the data of the user 🔴( data sanitization needed in post and patch apis)
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  const userId = req.body.userId;
+
+  try {
+    await User.findOneAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Update failed :" + error.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,51 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    requires: true,
+    minLength: 3,
+    maxLength: 20,
   },
   lastName: {
     type: String,
   },
   email: {
     type: String,
+    requires: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
   },
   password: {
     type: String,
+    requires: true,
   },
   age: {
     type: Number,
+    min: 18,
   },
   gender: {
     type: String,
-  }
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
+        throw new Error("Gender is invalid");
+      }
+    },
+  },
+  photoUrl: {
+    type: String,
+  },
+  about: {
+    type: String,
+    default:
+      "Seek within my friends you only need you. embrace every aspect of life. be empathatic and good to every person . all of us are stuck in this simulation and our time is limted this thing only make you realize that we should help each other and respectful towards each other ",
+  },
+  skills: {
+    type: [String, String],
+  },
+},{
+  timestamps:true,
 });
-
-
- 
 
 module.exports = mongoose.model("User", userSchema);
