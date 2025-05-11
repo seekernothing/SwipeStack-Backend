8f57cbe55ddfdbc40eacc7ad9c7b9cbf21f8c615
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-05-11 17:37:27 +0530

    consistant sending data

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -97,7 +97,7 @@ userRouter.get("/feed", userAuth, async (req, res) => {
       .skip(skip)
       .limit(limit);
 
-    res.send(users);
+    res.json({ data: users });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
