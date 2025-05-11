c6d7606fad21b3d3b8a520590adb099db414815b
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-05-11 17:20:31 +0530

    added pagination

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -63,6 +63,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
     const connectionRequests = await connectionRequestModel
       .find({
         $or: [
@@ -87,7 +91,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUsersFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA)
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
 
     res.send(users);
   } catch (error) {
